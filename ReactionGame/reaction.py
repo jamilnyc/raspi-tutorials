75d30f9ec0d0e3909e2468435068cdc6cc2ce2e0
Jamil <murshed.ahmed92@gmail.com>
2017-12-17 16:54:50 +0000

    Various game updates: Print instructions at the beginning Track average reaction time Some minor function cleanup

--- ReactionGame/reaction.py
+++ ReactionGame/reaction.py
@@ -9,38 +9,70 @@ led = LED(4)
 right_button = Button(15)
 left_button = Button(14)
 
-# Get user names from the command line input
-left_name = input('Left player name is ')
-right_name = input('Right player name is ')
-
 # For keeping track of the player's points
 left_points = 0
 right_points = 0
 
+# For tracking average reaction times
+left_reaction_total = 0
+right_reaction_total = 0
+
+def print_instructions():
+    print("######################################")
+    print('Reaction Game - 2 Players')
+    print('Directions:')
+    print('* Enter player names')
+    print('* Press your button when it lights up')
+    print('######################################')
+
+def print_hits(name, points):
+    message = name + ' won this round. ' + str(points) + ' '
+    if points > 1:
+        message += 'Points!'
+    else:
+        message += 'Point!'
+        
+    print (message)
+
+def print_misses(name):
+    message = 'Watch it, ' + name + '! It is not on.'
+    print(message)
+
 def pressed(button):
     # No points if it is not lit
     if not led.is_lit:
+        if button.pin.number == 14:
+            print_misses(left_name)
+        else:
+            print_misses(right_name)
         return
     
     # Declare global because the variables will be written
     global right_points
     global left_points
+    global right_reaction_total
+    global left_reaction_total
 
-    # TODO: Print averages of reaction time
+    # TODO: Track "false" hits (when LED is off)
 
     # variable led will only be read, no need to declare global
     led.off()
     press_time = time.time()
+    reaction_time = (press_time - lit_time)
+    reaction_average = 0
 
     if button.pin.number == 14:
         left_points += 1
-        print(left_name + ' won the round. ' + str(left_points) + ' Point(s)!')
+        left_reaction_total += reaction_time
+        reaction_average = left_reaction_total / left_points
+        print_hits(left_name, left_points)
     else:
         right_points += 1
-        print(right_name + ' won the round. ' + str(right_points) + ' Point(s)!')
+        right_reaction_total += reaction_time
+        reaction_average = right_reaction_total / right_points
+        print_hits(right_name, right_points)
 
-    reaction_time = (press_time - lit_time)
-    print('Reaction time: %.3f sec' % reaction_time)
+    print('Reaction time: %.3f sec (Avg. %.3f sec)' % (reaction_time, reaction_average))
 
 # Keep track of the time the LED turned on
 lit_time = 0
@@ -49,12 +81,18 @@ lit_time = 0
 right_button.when_pressed = pressed
 left_button.when_pressed = pressed
 
+print_instructions()
+
+# Get user names from the command line input
+left_name = input('Left player name is ')
+right_name = input('Right player name is ')
+
 # Loop forever . . . or until you hit CTRL+C
 while True:
     # Turn the LED on and off for random periods of time
     # between 5 and 10 seconds
     led.on()
     lit_time = time.time()
-    sleep(uniform(5, 10))
+    sleep(uniform(1, 4))
     led.off()
-    sleep(uniform(5, 10))
+    sleep(uniform(1, 4))
